94c83b5e315d7067ae5b68fb80c8b66fe9ea6284
stal722 <38084766+stal722@users.noreply.github.com>
2022-02-23 22:56:49 +0300

    Добавлен класс Point

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -1,5 +1,13 @@
 public class FirstProgram {
 	public static void main(String[] args) {
-		System.out.println("Hello, Java");
+
+		Point p = new Point(10, 10, 20, 20);
+
+
+		System.out.println(p.distance());
+
 	}
+
+
 }
+
---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,18 @@
+public class Point {
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+
+    public Point(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+
+    }
+
+    public double distance () {
+        return Math.sqrt((this.x2 - this.x1) * (this.x2 - this.x1) + (this.y2 - this.y1) * (this.y2 - this.y1));
+    }
+}
